eec7620540af4f0c557c9366a84886d1c47284a0
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-17 15:07:05 +0100

    script for creating data and tables

---
+++ README.md
@@ -0,0 +1,15 @@
+```
+clone
+```
+```
+cd path/Company
+```
+```
+ruby lib/data_creator.rb
+```
+```
+rackup
+```
+```
+http://localhost:9292/
+```
---
+++ lib/data_creator.rb
@@ -0,0 +1,48 @@
+require 'pg'
+
+class DatabaseCreator
+
+  def self.create_database
+    connection = PG.connect(dbname: 'postgres')
+    connection.exec('CREATE DATABASE clients')
+  end
+
+  def self.create_tables
+    connection = PG.connect(dbname: 'clients')
+    connection.exec('CREATE TABLE client_details (
+                    Company_ID int,
+                    Company_Name varchar,
+                    Company_Telephone varchar,
+                    Company_Representitive varchar,
+                    Company_Email varchar,
+                    Company_Address varchar,
+                    Company_Contract_Start_Date varchar,
+                    Company_Contract_End_Date varchar
+                    )')
+    connection = PG.connect(dbname: 'clients')
+    connection.exec('CREATE TABLE orders_summary (
+                    Company_ID int,
+                    Summary_ID int,
+                    Company_Name varchar,
+                    Number_Of_Orders int,
+                    Total_Revenue int,
+                    Company_Telephone varchar,
+                    Company_Email varchar
+                    )')
+    connection = PG.connect(dbname: 'clients')
+    connection.exec('CREATE TABLE orders (
+                    Row_ID int,
+                    Company_ID int,
+                    Company_Name varchar,
+                    Product_Reference int,
+                    Quantity int,
+                    Ordered_On_Date text,
+                    Delivery_Date text,
+                    Total_Revenue int,
+                    Company_Telephone varchar,
+                    Company_Email varchar
+                    )')
+  end
+end
+DatabaseCreator.create_database
+DatabaseCreator.create_tables
