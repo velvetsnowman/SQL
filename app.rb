d2d3750a0b147eee2b2572d7fa9fc962af1788f0
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-18 21:59:19 +0100

    can view all order summaries

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/company_details_retriever.rb'
+require './lib/all_orders_summary.rb'
 
 class Router < Sinatra::Base
   enable :sessions
@@ -7,6 +8,11 @@ class Router < Sinatra::Base
   get '/' do
     @company_sets = CompanyDetailsRetriever.get_all
     erb(:all_companies)
+  end
+
+  get '/order_summary' do
+    @order_summaries = AllOrdersSummary.get_all
+    erb(:all_order_summaries)
   end
 
 end
---
+++ lib/all_orders_summary.rb
@@ -0,0 +1,26 @@
+require 'pg'
+
+class AllOrdersSummary
+  attr_reader :summary_id,:company_id,:company_name,:number_of_orders,:total_revenue,:company_telephone,:company_email
+  def inititalize(summary_id,company_id,company_name,number_of_orders,total_revenue,company_telephone,company_email)
+    @summary_id = summary_id
+    @company_id = company_id
+    @company_name = company_name
+    @number_of_orders = number_of_orders
+    @total_revenue = total_revenue
+    @company_telephone = company_telephone
+    @company_email = company_email
+  end
+
+  def self.get_all
+    connection = PG.connect(dbname: 'clients')
+    result = connection.exec("SELECT * FROM all_orders_summary")
+    result.map { |row| AllOrdersSummary.new(row['summary_id'],
+                                            row['company_id'],
+                                            row['company_name'],
+                                            row['number_of_orders'],
+                                            row['total_revenue'],
+                                            row['company_telephone'],
+                                            row['company_email'],)}
+  end
+end
