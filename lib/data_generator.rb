3106293f97efc354f28fd52b0eed47559d7690cb
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-18 22:12:35 +0100

    updated self.creat_table_data for inputting initial data into order summaries

--- lib/data_generator.rb
+++ lib/data_generator.rb
@@ -10,6 +10,13 @@ class DataGenerator
   @contract_start = ['01/01/2015', '01/08/2009', '10/02/2014', '31/05/2016', '23/08/2015']
   @contract_end = ['01/01/2017', '01/08/2019', '10/02/2017', '31/05/2019', '23/08/2018']
 
+  @company_id = ['1', '2', '3', '4', '5']
+  @company_name = ['Monoprix', 'Super Z', 'A-Z', 'Carrefour', 'Asda']
+  @number_of_orders = ['45', '22', '54', '2', '62']
+  @total_revenue = [23,786, 5,967, 34,872, 230,89, 45,785]
+  @company_telephone = ['034569810', '579456345', '568956845', '963342234', '456789653']
+  @company_email = ['monoprixadmin@monoprix.com', 'superzmarket@gmail.com', 'a2z@alphabetmarket.com', 'carrefour@carrefour.com', 'asdacompany@gmail.com']
+
 
 
   def self.create_table_data
@@ -32,6 +39,20 @@ class DataGenerator
                         '#{@company_address[i]}',
                         '#{@contract_start[i]}',
                         '#{@contract_end[i]}');")
+      connection.exec("INSERT INTO all_orders_summary (
+                      Company_ID,
+                      Company_Name,
+                      Number_Of_Orders,
+                      Total_Revenue,
+                      Company_Telephone,
+                      Company_Email)
+                      VALUES (
+                        '#{@company_id[i]}',
+                        '#{@company_name[i]}',
+                        '#{@number_of_orders[i]}',
+                        '#{@total_revenue[i]}',
+                        '#{@company_telephone[i]}',
+                        '#{@company_email[i]}');")
       i += 1
     end
   end
